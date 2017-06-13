a9140d1e62318178af6203b1d920cbad85693f08
faraznaeem <faraznaeem@gmail.com>
2017-06-13 10:23:21 +0200

    två tester failar

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,19 +1,20 @@
-class Atm
-attr_accessor :funds
+require 'date'
 
-def initialize
-  @funds = 1000
+class Atm
+  attr_accessor :funds
+
+  def initialize
+    @funds = 1000
   end
 
   def withdraw(amount, account)
     case
-    when amount > account.balance
+  when amount > account.balance
       return
-    end
-
-    else
-    @funds -= amount
-    account.balance = account.balance - amount
-  { status: true, message: 'success', date: Date.today, amount: amount}
+  else
+      @funds -= amount
+      account.balance = account.balance - amount
+    { status: true, message: 'success', date: Date.today, amount: amount }
+  end
   end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,34 +1,24 @@
 require './lib/atm.rb'
+
 describe Atm do
-  let(:account) {instance_double('Account') }
 
-  it 'allow withdraw if account has enough balance.' do
-  # We need to tell the spec what to look for as the responce
-  # and store that in a variable called `expected_outcome`.
-  # Please note that we are omitting the `bills` part at the moment,
-  # We will modify this test and add that later.
-
-  expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
-
-  # We need to pass in two arguments to the `withdraw` method.
-  # The amount of money we want to withdraw AND the `account` object.
-  # The reason we pass in the `account` object is that the Atm needs
-  # to be able to access information about the `accounts` balance
-  # in order to be able to clear the transaction.
-  expect(subject.withdraw(45, account)).to eq expected_output
-end
+  let(:account) {instance_double('Account')} #the happy path section
 
   before do
-    allow(:account).to receive(:balance).and_return(100)
-    allow(account).to receive(:balance=)
+    allow(account).to receive(:balance).and_return(100)
   end
 
-  it 'has $1000 on initialize' do
+  it '1 allow subject withdraw if account has enough balance' do
+    expected_output = {status: true, message: 'success', date: Date.today, amount: 45}
+    expect(subject.withdraw(45, account)).to eq expected_output
+  end
+
+  it '2 has $1000 on initialize' do
     expect(subject.funds).to eq 1000
   end
 
-  it 'funds are reduced at withdraw' do
-    subject.withdraw 50
-    expect(subject).to eq 950
+  it '3 funds are reduced at withdraw' do
+    subject.withdraw(50, account)
+    expect(subject.funds).to eq 950
   end
 end
