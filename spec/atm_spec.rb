51f0ccfe07e842f569c8aea39d6f6d13ae76bc54
faraznaeem <faraznaeem@gmail.com>
2017-06-13 11:28:47 +0200

    pin_code tests are passing

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,7 +2,7 @@ require './lib/atm.rb'
 
 describe Atm do
 
-  let(:account) { instance_double('Account') } #the happy path section
+  let(:account) { instance_double('Account', pin_code: '1234') } #the happy path section
 
   before do
     allow(account).to receive(:balance).and_return(100)
@@ -11,7 +11,7 @@ describe Atm do
 
   it '1 allow subject withdraw if account has enough balance' do
     expected_output = {status: true, message: 'success', date: Date.today, amount: 45}
-    expect(subject.withdraw(45, account)).to eq expected_output
+    expect(subject.withdraw(45, '1234', account)).to eq expected_output
   end
 
   it '2 has $1000 on initialize' do
@@ -19,18 +19,18 @@ describe Atm do
   end
 
   it '3 funds are reduced at withdraw' do
-    subject.withdraw(50, account)
+    subject.withdraw(50, '1234', account)
     expect(subject.funds).to eq 950
   end
 
   it '4 rejects withdraw if account has insufficient funds' do
     expected_output = { status: false, message: 'insufficient funds', date: Date.today }
-    expect(subject.withdraw(105, account)).to eq expected_output
+    expect(subject.withdraw(105, '1234', account)).to eq expected_output
   end
 
   it '5 rejects withdraw when ATM has insufficient funds' do
     subject.funds = 50
     expected_output = {status: false, message: 'insufficient funds in atm', date: Date.today }
-    expect(subject.withdraw(100, account)).to eq expected_output
+    expect(subject.withdraw(100, '1234', account)).to eq expected_output
   end
 end
