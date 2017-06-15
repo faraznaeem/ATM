97072880203cc602ceb5856b9facfe16b3356b52
faraznaeem <faraznaeem@gmail.com>
2017-06-15 10:41:58 +0200

    got the exp_date to work

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,16 @@
+require 'date'
 class Account
+  attr_accessor :balance #fucking shit why dont you work
+  attr_accessor :pin_code
+  attr_accessor :exp_date
 
-  def attr_accessor
-    :pin_code
+  STANDARD_VALIDITY_YRS = 5
 
-    def initialize
+  def initialize
       @pin_code = make_pin
-    end
+      @balance = 0
+      @exp_date = set_exp_date
+  end
 
   private
 
@@ -13,5 +18,9 @@ class Account
     rand(1000..9999) #look up the difference between two dots and three?
   end
 
+  def set_exp_date
+    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
   end
+
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,12 +1,21 @@
 require './lib/account.rb'
 
 describe Account do
-#place numbers on tests to increase readability
+#placed numbers on tests to increase readability
 
   it '1 check length of a number' do
     number = 1234
     number_length = Math.log10(number).to_i + 1
     expect(number_length).to eq 4
   end
+
+  it '2 check the account balance on initialize' do
+    expect(subject.balance).to eq 0
+  end
+
+  it '3 is expected to have an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
+end
 
 end
