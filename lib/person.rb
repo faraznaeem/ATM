024158ab452eb4147d1f7264685fc17b03c2b18f
faraznaeem <faraznaeem@gmail.com>
2017-06-15 18:26:34 +0200

    added missing name

--- lib/person.rb
+++ lib/person.rb
@@ -3,11 +3,12 @@ require './lib/atm'
 
 class Person
   attr_accessor :name
-  #attr_accessor :cash
+  attr_accessor :cash
   #attr_accessor :account
 
     def initialize(attrs = {})
       @name = set_name(attrs[:name])
+      @cash = 0
     end
 
   private
@@ -15,4 +16,10 @@ class Person
     def set_name(name)
      name.nil? ? missing_name : name
     end
+
+    def missing_name
+    raise ArgumentError, 'A name is required' #dont understand 'raise'
+    end
+
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -8,4 +8,13 @@ subject { described_class.new(name: 'Faraz') }
   it '1 is expected to have a :name on initialize' do
       expect(subject.name).not_to be nil
   end
+
+  it '2 is expected to raise error if no name is set' do
+    expect { described_class.new }.to raise_error 'A name is required'
+  end
+
+  it '3 is expected to have a :cash attribute with value of 0 on initialize' do
+    expect(subject.cash).to eq 0
+  end
+
 end
